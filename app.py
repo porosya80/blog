fc08253e6fb8d119acb082602a683cdcc5797efc
Dmytriy Myk <porosya@gmail.com>
2018-06-11 22:08:27 +0300

    working ... :)

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from config import Configuration
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
+
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run()
+
+# if __name__ == '__main__':
+#     app.run()
